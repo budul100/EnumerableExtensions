83bf2c70b066d7ee2a496ca6bd0836cbfce3cdb6
Marek Grochowski <marekgrochowski@web.de>
2020-05-14 13:52:42 +0200

    SplitAtChange test added

--- EnumerableExtensionsTests/Tests.cs
+++ EnumerableExtensionsTests/Tests.cs
@@ -1,15 +1,42 @@
+using EnumerableExtensions;
 using NUnit.Framework;
+using System.Linq;
 
 namespace EnumerableExtensionsTests
 {
     public class Tests
     {
-
+        #region Public Methods
 
         [Test]
-        public void Test1()
+        public void SpliAtChange()
         {
-            Assert.Pass();
+            var values = new TestObject[] {
+                new TestObject { Value1 = 1 },
+                new TestObject { Value1 = 1 },
+                new TestObject { Value1 = 2 },
+                new TestObject { Value1 = 1 }
+            };
+
+            var result = values
+                .SplitAtChange(v => v.Value1).ToArray();
+
+            Assert.IsTrue(result.Count() == 3);
         }
+
+        #endregion Public Methods
+
+        #region Private Classes
+
+        private class TestObject
+        {
+            #region Public Properties
+
+            public int Value1 { get; set; }
+
+            #endregion Public Properties
+        }
+
+        #endregion Private Classes
     }
 }
