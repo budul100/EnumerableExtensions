cd03c555ea46c6f3f970e0bf5d42d31deb59987a
budul <MarekGrochowski@web.de>
2022-07-27 19:40:58 +0200

    Chunked function added

--- EnumerableExtensions/Group.cs
+++ EnumerableExtensions/Group.cs
@@ -9,44 +9,48 @@ namespace EnumerableExtensions
     {
         #region Public Methods
 
-        public static IEnumerable<IEnumerable<T>> ChunkAfter<T>(this IEnumerable<T> items, Func<T, bool> predicate)
+        public static IEnumerable<IEnumerable<T>> ChunkAfter<T>(this IEnumerable<T> items,
+            Func<T, bool> predicate)
         {
+            var result = new List<T>();
+
             if (predicate == default)
             {
                 throw new ArgumentNullException(nameof(predicate));
             }
 
-            var result = new List<T>();
-
             foreach (var item in items.IfAny())
             {
                 result.Add(item);
 
-                if (result.Any() && (predicate?.Invoke(item) ?? false))
+                if (result.Count > 0
+                    && predicate.Invoke(item))
                 {
                     yield return result;
                     result = new List<T>();
                 }
             }
 
-            if (result.Any())
+            if (result.Count > 0)
             {
                 yield return result;
             }
         }
 
-        public static IEnumerable<IEnumerable<T>> ChunkBefore<T>(this IEnumerable<T> items, Func<T, bool> predicate)
+        public static IEnumerable<IEnumerable<T>> ChunkBefore<T>(this IEnumerable<T> items,
+            Func<T, bool> predicate)
         {
+            var result = new List<T>();
+
             if (predicate == default)
             {
                 throw new ArgumentNullException(nameof(predicate));
             }
 
-            var result = new List<T>();
-
             foreach (var item in items.IfAny())
             {
-                if (result.Any() && (predicate?.Invoke(item) ?? false))
+                if (result.Count > 0
+                    && predicate.Invoke(item))
                 {
                     yield return result;
                     result = new List<T>();
@@ -55,31 +59,34 @@ namespace EnumerableExtensions
                 result.Add(item);
             }
 
-            if (result.Any())
+            if (result.Count > 0)
             {
                 yield return result;
             }
         }
 
-        public static IEnumerable<IEnumerable<T>> Framed<T>(this IEnumerable<T> items, Func<T, bool> predicate)
+        public static IEnumerable<IEnumerable<T>> Chunked<T>(this IEnumerable<T> items,
+            Func<T, bool> predicate)
         {
+            var result = new List<T>();
+
             if (predicate == default)
             {
                 throw new ArgumentNullException(nameof(predicate));
             }
 
-            var result = new List<T>();
+            var hasContent = false;
 
             foreach (var item in items.IfAny())
             {
-                if (result.Any()
-                    || (predicate?.Invoke(item) ?? false))
+                if (hasContent || result.Count() == 0 || !predicate.Invoke(item))
                 {
                     result.Add(item);
+                    hasContent = hasContent || !predicate.Invoke(item);
                 }
 
-                if (result.Count > 1
-                    && (predicate?.Invoke(item) ?? false))
+                if (hasContent
+                    && predicate.Invoke(item))
                 {
                     yield return result;
 
@@ -87,8 +94,25 @@ namespace EnumerableExtensions
                     {
                         item
                     };
+
+                    hasContent = false;
                 }
             }
+
+            if (hasContent)
+            {
+                yield return result;
+            }
+        }
+
+        public static IEnumerable<IEnumerable<T>> Framed<T>(this IEnumerable<T> items,
+            Func<T, bool> predicate)
+        {
+            var result = items.Chunked(predicate)
+                .Where(g => predicate.Invoke(g.First())
+                    && predicate.Invoke(g.Last()));
+
+            return result;
         }
 
         public static IEnumerable<IEnumerable<T>> GroupByHash<T, TProperty>(this IEnumerable<T> items,
@@ -107,19 +131,19 @@ namespace EnumerableExtensions
 
         public static IEnumerable<IEnumerable<T>> SplitAt<T>(this IEnumerable<T> items, Func<T, bool> predicate)
         {
+            var result = new List<T>();
+
             if (predicate == default)
             {
                 throw new ArgumentNullException(nameof(predicate));
             }
-
-            var result = new List<T>();
 
             foreach (var item in items.IfAny())
             {
                 result.Add(item);
 
                 if (result.Count > 1
-                    && (predicate?.Invoke(item) ?? false))
+                    && predicate.Invoke(item))
                 {
                     yield return result;
 
@@ -136,14 +160,15 @@ namespace EnumerableExtensions
             }
         }
 
-        public static IEnumerable<IEnumerable<T>> SplitAtChange<T, TProperty>(this IEnumerable<T> items, Func<T, TProperty> property)
+        public static IEnumerable<IEnumerable<T>> SplitAtChange<T, TProperty>(this IEnumerable<T> items,
+            Func<T, TProperty> property)
         {
+            var result = new List<T>();
+
             if (property == default)
             {
                 throw new ArgumentNullException(nameof(property));
             }
-
-            var result = new List<T>();
 
             var last = default(TProperty);
 
@@ -153,7 +178,7 @@ namespace EnumerableExtensions
                     ? property(item)
                     : default;
 
-                if (result.Any()
+                if (result.Count > 0
                     && !current.IsEqual(last))
                 {
                     yield return result;
@@ -164,7 +189,7 @@ namespace EnumerableExtensions
                 last = current;
             }
 
-            if (result.Any())
+            if (result.Count > 0)
             {
                 yield return result;
             }
--- EnumerableExtensionsTests/Group.cs
+++ EnumerableExtensionsTests/Group.cs
@@ -11,6 +11,114 @@ namespace EnumerableExtensionsTests
         #region Public Methods
 
         [Test]
+        public void Chunked()
+        {
+            disposalCount = 0;
+
+            var result = GetWithDisposal(
+                new TestObject(1),
+                new TestObject(1),
+                new TestObject(2),
+                new TestObject(1))
+                .Chunked(v => v.Value1 == 1).ToArray();
+
+            Assert.IsTrue(result.Length == 1);
+            Assert.IsTrue(result[0].Count() == 3);
+
+            Assert.True(disposalCount == 1);
+        }
+
+        [Test]
+        public void ChunkedWithoutEnd()
+        {
+            disposalCount = 0;
+
+            var result = GetWithDisposal(
+                new TestObject(1),
+                new TestObject(1),
+                new TestObject(2),
+                new TestObject(1),
+                new TestObject(1),
+                new TestObject(2))
+                .Chunked(v => v.Value1 == 1).ToArray();
+
+            Assert.IsTrue(result.Length == 2);
+            Assert.IsTrue(result[0].Count() == 3);
+            Assert.IsTrue(result.Last().Count() == 2);
+
+            Assert.True(disposalCount == 1);
+        }
+
+        [Test]
+        public void ChunkedWithoutStart()
+        {
+            disposalCount = 0;
+
+            var result = GetWithDisposal(
+                new TestObject(2),
+                new TestObject(1),
+                new TestObject(1),
+                new TestObject(1),
+                new TestObject(2),
+                new TestObject(1))
+                .Chunked(v => v.Value1 == 1).ToArray();
+
+            Assert.IsTrue(result.Length == 2);
+            Assert.IsTrue(result[0].Count() == 2);
+            Assert.IsTrue(result.Last().Count() == 3);
+
+            Assert.True(disposalCount == 1);
+        }
+
+        [Test]
+        public void Framed()
+        {
+            disposalCount = 0;
+
+            var result = GetWithDisposal(
+                new TestObject(1),
+                new TestObject(1),
+                new TestObject(2),
+                new TestObject(1))
+                .Framed(v => v.Value1 == 1).ToArray();
+
+            Assert.IsTrue(result.Length == 1);
+            Assert.IsTrue(result[0].Count() == 3);
+
+            Assert.True(disposalCount == 1);
+        }
+
+        [Test]
+        public void FramedWithoutEnd()
+        {
+            disposalCount = 0;
+
+            var result = GetWithDisposal(
+                new TestObject(1),
+                new TestObject(2))
+                .Framed(v => v.Value1 == 1).ToArray();
+
+            Assert.IsTrue(result.Length == 0);
+
+            Assert.True(disposalCount == 1);
+        }
+
+        [Test]
+        public void FramedWithoutStart()
+        {
+            disposalCount = 0;
+
+            var result = GetWithDisposal(
+                new TestObject(2),
+                new TestObject(1))
+                .Framed(v => v.Value1 == 1).ToArray();
+
+            Assert.IsTrue(result.Length == 0);
+
+            Assert.True(disposalCount == 1);
+        }
+
+        [Test]
         public void SplitAtChange()
         {
             disposalCount = 0;
@@ -22,7 +130,7 @@ namespace EnumerableExtensionsTests
                 new TestObject(1))
                 .SplitAtChange(v => v.Value1).ToArray();
 
-            Assert.IsTrue(result.Count() == 3);
+            Assert.IsTrue(result.Length == 3);
 
             Assert.True(disposalCount == 1);
         }
